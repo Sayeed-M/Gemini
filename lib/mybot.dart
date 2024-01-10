df08c65cd486f666ca0cba152565f02e1163ee1e
unknown <mohdsayeed1092@gmail.com>
2024-01-10 17:52:57 +0530

    first commit

---
+++ lib/mybot.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Chatbot extends StatefulWidget {
+  const Chatbot({super.key});
+
+  @override
+  State<Chatbot> createState() => _ChatbotState();
+}
+
+class _ChatbotState extends State<Chatbot> {
+  ChatUser myself = ChatUser(id: '1', firstName: 'sayeed');
+  ChatUser bot = ChatUser(id: '2', firstName: 'Ranger');
+
+  List<ChatMessage> allMessage = [];
+  List<ChatUser> typing = [];
+
+  final oururl =
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDgFsfDzBLtEVt61-xyYRRkZtOUuI8bh5A';
+  final header = {'Content-Type': 'application/json'};
+
+  getdata(ChatMessage m) async {
+    typing.add(bot);
+    allMessage.insert(0, m);
+    setState(() {});
+    var data = {
+      "contents": [
+        {
+          "parts": [
+            {"text": m.text}
+          ]
+        }
+      ]
+    };
+
+    await http
+        .post(Uri.parse(oururl), headers: header, body: jsonEncode(data))
+        .then((value) {
+      if (value.statusCode == 200) {
+        var result = jsonDecode(value.body);
+        print(result['candidates'][0]['content']['parts'][0]['text']);
+
+        ChatMessage m1 = ChatMessage(
+            text: result['candidates'][0]['content']['parts'][0]['text'],
+            user: bot,
+            createdAt: DateTime.now());
+
+        allMessage.insert(0, m1);
+      } else {
+        print("error occured");
+      }
+    }).catchError((e) {});
+    typing.remove(bot);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('            MS-Ranger'),
+        backgroundColor: Colors.lightBlueAccent,
+        titleTextStyle: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+      ),
+      body: DashChat(
+          typingUsers: typing,
+          currentUser: myself,
+          onSend: (ChatMessage m) {
+            getdata(m);
+          },
+          messages: allMessage),
+    );
+  }
+}
